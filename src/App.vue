38577ab66966fd13f053139c648f2f38c3a86947
benaissa <benaissam1999@gmail.com>
2023-11-02 21:32:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <NavBar />
+    <v-content class="ma-4">
+      <router-view></router-view>
+    </v-content>
+    <AppFooter />
+  </v-app>
+</template>
+<script>
+
+import NavBar from "@/components/NavBar";
+import AppFooter from "@/components/AppFooter";
+export default {
+  name: "App",
+
+  components: {
+   NavBar,
+   AppFooter,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+
+</script>
